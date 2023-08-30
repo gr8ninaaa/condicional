3cc49883d505f00acce412d26b238f3dbc01b019
nina <134503742+gr8ninaaa@users.noreply.github.com>
2023-08-30 09:54:03 -0300

    Add files via upload

---
+++ condicional/condicional.js
@@ -0,0 +1,27 @@
+function media(){
+    var val1= document.getElementById('valor1').value;
+    var val2= document.getElementById('valor2').value;
+    var media= (parseInt(val1) + parseInt(val2)) /2;
+  
+
+    if(media >= 6)
+    if(media==10)
+     alert("O aluno atigiu a media" + " e foi aprovado com sucesso!");
+     else 
+     alert("Parabéns, aprovado! Media  "+ media);
+     else
+     alert("Volta ano que vem! ")
+}
+function comparar(){
+    var val1= document.getElementById('valor1').value;
+    var val2= document.getElementById('valor2').value;
+
+    if(val1==val2)
+    if (val1 > val2)
+        alert('Os valores são iguais')
+    else
+        alert('O valor 1 é maior que o valor 2')
+    else
+        alert('O valor 2 é maior que o valor 1')
+
+}
